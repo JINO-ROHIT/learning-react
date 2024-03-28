6ddc4dd00f7acb2dac84129aab0f54422ff7ab7c
JINO-ROHIT <find.jinorohit@gmail.com>
2024-03-28 19:00:19 +0530

    adding single clicks

--- accordion/src/components/index.jsx
+++ accordion/src/components/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
 const Accordian = () => {
+
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId){
+    setSelected(selected === getCurrentId ? null : getCurrentId)
+  }
+
   return (
-    <div>Heyyoooo</div>
+    <div className='accordian'>
+      {data && data.length > 0 ? (
+        data.map((dataItem) => (
+
+          <div className='item'>
+
+            <div onClick = {() => handleSingleSelection(dataItem.id)} className='title'>
+              <h3> {dataItem.question} </h3>
+              <span> + </span>
+            </div>
+
+            <div className='acc-content'>
+              {selected === dataItem.id ? 
+              <div> {dataItem.answer} </div> : null}
+            </div>
+
+
+          </div>
+        )))
+
+        : (<div> Not found </div>)
+
+      }
+    </div>
   )
 }
 
-export default Accordian
+export default Accordian;
