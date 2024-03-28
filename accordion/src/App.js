f98bb430675a57f4eb6fbd515384a325b575c5f2
JINO-ROHIT <find.jinorohit@gmail.com>
2024-03-28 18:06:38 +0530

    accordion baseline

---
+++ accordion/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordian from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian/>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ accordion/src/components/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Accordian = () => {
+  return (
+    <div>Heyyoooo</div>
+  )
+}
+
+export default Accordian
