017c0636e3ccf1ffdf617a222ad40f85b2c15142
JINO-ROHIT <find.jinorohit@gmail.com>
2024-03-28 19:41:19 +0530

    adding multi click

--- accordion/src/components/index.jsx
+++ accordion/src/components/index.jsx
@@ -4,35 +4,56 @@ import data from './data';
 const Accordian = () => {
 
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-  function handleSingleSelection(getCurrentId){
+  function handleSingleSelection(getCurrentId) {
     setSelected(selected === getCurrentId ? null : getCurrentId)
   }
 
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple]
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+
+    if (findIndexOfCurrentId !== -1) {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    } else {
+      copyMultiple.push(getCurrentId);
+    }
+    setMultiple(copyMultiple);
+  }
+
   return (
-    <div className='accordian'>
-      {data && data.length > 0 ? (
-        data.map((dataItem) => (
+    <div className='acc-wrapper'>
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable multi-select</button>
+      <div className='accordian'>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
 
-          <div className='item'>
+            <div className='item'>
 
-            <div onClick = {() => handleSingleSelection(dataItem.id)} className='title'>
-              <h3> {dataItem.question} </h3>
-              <span> + </span>
+              <div onClick={enableMultiSelection
+                ? () => handleMultiSelection(dataItem.id)
+                : () => handleSingleSelection(dataItem.id)} className='title'>
+                <h3> {dataItem.question} </h3>
+                <span> + </span>
+              </div>
+
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                  <div className="acc-content ">{dataItem.answer}</div>
+                )
+                : selected === dataItem.id && (
+                  <div className="acc-content ">{dataItem.answer}</div>
+                )}
+
             </div>
+          )))
 
-            <div className='acc-content'>
-              {selected === dataItem.id ? 
-              <div> {dataItem.answer} </div> : null}
-            </div>
+          : (<div> Not found </div>)
 
-
-          </div>
-        )))
-
-        : (<div> Not found </div>)
-
-      }
+        }
+      </div>
     </div>
   )
 }
